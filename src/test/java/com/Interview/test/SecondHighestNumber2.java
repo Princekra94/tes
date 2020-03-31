3e6ec2febdcf97265052a1e55c2af84475caf739
Prince Kumar <prince@Princes-MacBook-Pro.local>
2020-03-31 13:05:04 +0530

    Second highest number2

---
+++ src/test/java/com/Interview/test/SecondHighestNumber2.java
@@ -0,0 +1,22 @@
+package com.Interview.test;
+
+import java.util.Arrays;
+
+public class SecondHighestNumber2 {
+
+	public static void main(String[] args) {
+
+		int number[]={44,66,99,77,33,22,55,90,98,89,54};  
+
+		Arrays.sort(number);
+
+		int length = number.length;
+		System.out.println(length);
+
+		System.out.println("Second Highest Number = "+ number[number.length-2]);
+		
+
+
+	}
+
+}
